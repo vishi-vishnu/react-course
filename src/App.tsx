01dd890e3502605148142ad77c67f41782c3c815
vishi-visihnu <vish08vishnu@gmail.com>
2024-06-21 18:18:04 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
+import { CreatePost } from "./pages/create-post/Create-Post";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgGhoECRAKNurIp_kTyiVuGkQzYV0t3-E",
+  authDomain: "react-course-fcee3.firebaseapp.com",
+  projectId: "react-course-fcee3",
+  storageBucket: "react-course-fcee3.appspot.com",
+  messagingSenderId: "113616135335",
+  appId: "1:113616135335:web:ddefc38ac445ed8265a782",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth =getAuth(app);
+export const provider = new GoogleAuthProvider();
